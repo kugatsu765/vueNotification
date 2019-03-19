daa30371137450785435f14cec7f1db8bec24705
Kugatsu <rgabel@pm.com>
2019-03-19 15:26:34 +0100

    📄 (Sample) Start real documentation.

--- src/App.vue
+++ src/App.vue
@@ -17,17 +17,42 @@
 
     <v-content>
       <div id="app">
-        <v-btn color="primary" @click="notif('new')">Primary</v-btn>
-        <br>
-        <v-btn color="success" @click="notif('success')">success</v-btn>
-        <br>
-        <v-btn color="error" @click="notif('error')">error</v-btn>
-        <br>
-        <v-btn color="warning" @click="notif('warning')">warning</v-btn>
-        <br>
-        <v-btn color="info" @click="notif('info')">info</v-btn>
-        <br>
-        <v-btn @click="remove()">Remove All</v-btn>
+        <v-container grid-list-md text-xs-center>
+          <v-layout row wrap>
+            <v-flex xs12>Position
+              <v-select
+                v-model="position"
+                :items="positions"
+                label="Position"
+                :disabled="buttonClicked"
+              ></v-select>
+            </v-flex>
+            <v-flex xs12>Timer
+              <v-slider v-model="timer" thumb-label="always"></v-slider>
+            </v-flex>
+            <v-flex xs12>infinteTimer
+              <v-checkbox
+                v-model="infiniteTimer"
+                label="Allow you to display a notification whitout timer"
+              ></v-checkbox>
+            </v-flex>
+            <v-flex xs12>showLeftIcn
+              <v-checkbox v-model="showLeftIcn" label="Allow you to display hide the left icone"></v-checkbox>
+            </v-flex>
+            <v-flex xs12>
+              <v-btn color="primary" @click="notif('new')">Primary</v-btn>
+              <v-btn color="success" @click="notif('success')">success</v-btn>
+              <v-btn color="error" @click="notif('error')">error</v-btn>
+              <v-btn color="warning" @click="notif('warning')">warning</v-btn>
+              <v-btn color="info" @click="notif('info')">info</v-btn>
+              <v-btn @click="remove()">Remove All</v-btn>
+            </v-flex>
+
+            <v-flex xs12>
+              <p>{{getConfig}}</p>
+            </v-flex>
+          </v-layout>
+        </v-container>
       </div>
     </v-content>
   </v-app>
@@ -39,14 +64,43 @@ export default {
   components: {},
   data: function() {
     return {
-      count: 0
+      positions: [
+        "topLeft",
+        "topCenter",
+        "topRight",
+        "bottomLeft",
+        "bottomCenter",
+        "bottomRight"
+      ],
+      position: "topRight",
+      timer: 5,
+      infiniteTimer: false,
+      showLeftIcn: true,
+      count: 0,
+      buttonClicked: false,
+      config: {}
     };
+  },
+  computed: {
+    getConfig: function() {
+      return (this.config = {
+        position: this.position,
+        timer: this.timer,
+        infiniteTimer: this.infiniteTimer,
+        showLeftIcn: this.showLeftIcn
+      });
+    }
   },
   methods: {
     notif: function(type) {
       this.count++;
+
+      let config = this.getConfig;
+      this.buttonClicked = true;
+
       this.$notification[type]({
-        message: "Sample of notification throw with number " + this.count
+        message: "Sample of notification throw with number " + this.count,
+        ...config
       });
     },
     remove: function() {
