9b945d5431a899bb743292563fc35bcd375acf9e
Kugatsu <rgabel@pm.com>
2019-03-19 10:16:35 +0100

    🚚 Update plugin folder name

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import Vue from "vue";
 import App from "./App.vue";
-import NotificationVuejs from "./NotificationsVuejs";
+import NotificationVuejs from "./VueNotification/index";
 
 Vue.config.productionTip = false;
 Vue.use(NotificationVuejs, {
